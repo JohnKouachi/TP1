e2da85783379d827ffbaa23e6a5269c047fecf17
JohnKouachi <j.rafael.faria543@gmail.com>
2020-03-10 20:57:42 +0000

    Primeiro Commit tp1

---
+++ src/TP1/Petroleiro.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP1;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Petroleiro extends Navio {
+   
+    private float carga;
+    
+    public Petroleiro(String matricula) {
+        super(matricula);
+    }
+    
+    public float getCarga() {
+        return carga;
+    }
+    
+    public void setCarga(float carga) {
+        this.carga = carga;
+    }
+
+}
+
+
---
+++ src/TP1/PortaContentores.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP1;
+
+/**
+ *
+ * @author Jorge
+ */
+public class PortaContentores extends Navio {
+    
+    private int noContentores;
+
+    public PortaContentores(String matricula) {
+        super(matricula);
+    }
+    
+    public int getNoContentores() {
+        return noContentores;
+    }
+
+    public void setNoContentores(int noContentores) {
+        this.noContentores = noContentores;
+    }
+    
+}
+
